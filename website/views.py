a9cd1a072e9b7a77e14d72c2e75c4bb54e5da8c7
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-10-12 01:19:22 -0400

    added functionality for home page

--- website/views.py
+++ website/views.py
@@ -1,13 +1,21 @@
 from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
 from . import db
+from .webscraping import scrape, toItems
 
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=["GET", "POST"])
 @login_required
 def home():
-    return render_template("home.html", user=current_user)
+    if request.method == "POST":
+        search = request.form.get("search")
+
+        #function scrape returns a list of search results
+        items = scrape(search)
+        itemObjects = toItems(items)
+
+    return render_template("home.html", user=current_user, items=itemObjects)
 
 @views.route('/saved', methods=["GET"])
 @login_required
