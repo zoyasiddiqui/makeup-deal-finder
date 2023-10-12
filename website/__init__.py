fd9895bd136a1b4f5b04a2b5623adcfe2f28d2c2
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-10-11 23:47:01 -0400

    moved code to correct file

---
+++ website/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET KEY'] = "secret key"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///database.db'
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+    
+    return app
