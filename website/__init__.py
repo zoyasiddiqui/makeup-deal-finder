743068b38823e4b380d5d210b2152658c5469be6
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-10-12 00:19:04 -0400

    add functionality for app

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager, login_manager
 
 db = SQLAlchemy()
 
@@ -11,5 +13,21 @@ def create_app():
 
     with app.app_context():
         db.create_all()
-    
+
+    from .views import views
+    from .auth import auth
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
+    from .models import User
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+
     return app
